45bc169f9951179223c75673e415cb5797689466
Alfur <zbsrebyata@gmail.com>
2023-03-29 12:25:03 +0300

    Finished task 4 -- using zip() func

---
+++ python_tasks/task_4/task_4.py
@@ -0,0 +1,7 @@
+"""Task 4: use zip() function to make pairs from list1 & list2 items"""
+
+list1 = ['Oleg', 'Vasya', 'Anna']
+list2 = ['Ivanov', 'Sidorov', 'Petrova']
+
+zipped = zip(list1, list2)
+print(list(zipped))
